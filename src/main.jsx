40b065194eeef34bd1778f6365e97d9888c40828
Emin Azeroglu <azeroglu.developer@gmail.com>
2023-09-10 12:41:20 +0400

    fix: Change Hash Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalProvider from "./providers/GlobalProvider";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './assets/css/app.css'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <HashRouter>
         <GlobalProvider />
-    </BrowserRouter>
+    </HashRouter>
 );
